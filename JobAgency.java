7168da911ed8fe869e6b87c755c4b2d6ea8e8ad1
Chittajallu Chinmai Ganesh <46193776+chcg823@users.noreply.github.com>
2022-03-18 21:50:32 +0530

    Create JobAgency.java

---
+++ JobAgency.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class CompanyJobRepository {
+
+	static String qual[] = { "B.E", "M.S", "PhD"};
+	static ArrayList<String> qualification = new ArrayList<String>(Arrays.asList(qual));
+
+	public static String getJobPrediction(int age, String highestQualification) {	
+		try {
+			if(age<19)
+				throw new NotEligibleException("You are underage for any job");
+			else {
+				try {
+					if(!qualification.contains(highestQualification))
+						throw new NotEligibleException("We do not have any job that matches your qualifications");
+					else if(age>=21 && highestQualification.equals("B.E"))
+						return "We have openings for junior developer";
+					else if(age>=26 && !highestQualification.equals("B.E"))
+						return "We have openings for senior developer";
+					else
+						return "Sorry we have no openings for now";
+				} catch (NotEligibleException e) {
+					return e.getMessage();
+				}
+			}
+		} catch (NotEligibleException e) {
+			return e.getMessage();
+		}
+	}
+
+}
+
+public class Source {
+
+	public String searchForJob(int age, String highestQualification) {
+		try {
+			throw new InvalidAgeException("The age entered is not typical for a human being");
+		} catch (InvalidAgeException e){
+			return e.getMessage();
+		}
+	}
+
+	public static void main(String args[] )  {
+		/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	}
+}
+class NotEligibleException extends Exception {
+	public NotEligibleException(String message) {
+		super(message);
+	}
+}
+
+class InvalidAgeException extends Exception {
+	public InvalidAgeException(String message) {
+		super(message);
+	}
+}
