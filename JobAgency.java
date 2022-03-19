e77ee326f95d6d4388be6c9444979286d482488f
Chittajallu Chinmai Ganesh <46193776+chcg823@users.noreply.github.com>
2022-03-19 10:32:35 +0530

    Update JobAgency.java

--- JobAgency.java
+++ JobAgency.java
@@ -9,26 +9,18 @@ class CompanyJobRepository {
 	static String qual[] = { "B.E", "M.S", "PhD"};
 	static ArrayList<String> qualification = new ArrayList<String>(Arrays.asList(qual));
 
-	public static String getJobPrediction(int age, String highestQualification) {	
-		try {
-			if(age<19)
+	public static String getJobPrediction(int age, String highestQualification) throws NotEligibleException{	
+		if(age<19)
 				throw new NotEligibleException("You are underage for any job");
-			else {
-				try {
-					if(!qualification.contains(highestQualification))
-						throw new NotEligibleException("We do not have any job that matches your qualifications");
-					else if(age>=21 && highestQualification.equals("B.E"))
-						return "We have openings for junior developer";
-					else if(age>=26 && !highestQualification.equals("B.E"))
-						return "We have openings for senior developer";
-					else
-						return "Sorry we have no openings for now";
-				} catch (NotEligibleException e) {
-					return e.getMessage();
-				}
-			}
-		} catch (NotEligibleException e) {
-			return e.getMessage();
+		else {
+			if(!qualification.contains(highestQualification))
+					throw new NotEligibleException("We do not have any job that matches your qualifications");
+			else if(age>=21 && highestQualification.equals("B.E"))
+				return "We have openings for junior developer";
+			else if(age>=26 && !highestQualification.equals("B.E"))
+				return "We have openings for senior developer";
+			else
+				return "Sorry we have no openings for now";
 		}
 	}
 
@@ -36,12 +28,10 @@ class CompanyJobRepository {
 
 public class Source {
 
-	public String searchForJob(int age, String highestQualification) {
-		try {
-			throw new InvalidAgeException("The age entered is not typical for a human being");
-		} catch (InvalidAgeException e){
-			return e.getMessage();
-		}
+	public String searchForJob(int age, String highestQualification) throws NotEligibleException {
+		if(age<=0 || age>=200)
+			throw new NotEligibleException("The age entered is not typical for a human being");
+		return CompanyJobRepository.getJobPrediction(age, highestQualification);
 	}
 
 	public static void main(String args[] )  {
